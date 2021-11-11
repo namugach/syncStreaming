571be611b7ef0443f99838f51783517e9cd7a910
namugach <namugach@gmail.com>
2021-11-11 19:14:56 +0900

    room과 join을 사용하여 브로드 케스팅

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,17 +5,25 @@ app = {
 		changeBgColor(e) {
 			const targetStyle = e.target.style;
 			targetStyle.backgroundColor = targetStyle.backgroundColor === "red" ? "#333" : "red";
+		},
+		seekedHandle(e) {
+			socket.emit("seeked", e.target.currentTime);
 		}
 	},
 	get: {
 		bgColor(e) {
 			return e.target.style.backgroundColor;
+		},
+		videoElem() {
+			return document.getElementById("videoDisplay");
 		}
 	}
-};
+},
+videoElem = app.get.videoElem();
 
 
 socket.emit("join");
+
 box.addEventListener("click", e => {
 	e.preventDefault();
 	app.handle.changeBgColor(e);
@@ -29,3 +37,149 @@ socket.on("boxClick", info => {
 	const box = document.getElementById(info.id);
 	box.style.backgroundColor = info.color;
 });
+
+
+if(true) {
+	function playHandle(e) {
+		socket.emit("play", () => {
+			const videoElem = document.getElementById("videoElem");
+			// videoElem.play();
+			console.log(1);
+		});
+	}
+	function pauseHandle(e) {
+		socket.emit("pause", () => {
+			const videoElem = document.getElementById("videoElem");
+			console.log(2);
+			// videoElem.pause();
+		});
+	}
+	videoElem.addEventListener("play", playHandle);
+	videoElem.addEventListener("pause", pauseHandle);
+	socket.on("play", () => {
+		console.log("this is broadcast play");
+		videoElem.play();
+	});
+	socket.on("pause", () => {
+		console.log("this is broadcast pause");
+		videoElem.pause();
+	});
+}
+
+if(false) {
+	function playHandle(e) {
+		socket.emit("play");
+	}
+	function pauseHandle(e) {
+		socket.emit("pause");
+	}
+	
+	function onPlay() {
+		if(videoElem.paused) {
+			videoElem.play();
+		}
+	}
+	function onPaues() {
+		if(!videoElem.paused) {
+			videoElem.pause();
+		}
+	}
+	videoElem.addEventListener("play", playHandle);
+	socket.on("play", onPlay);
+	
+	videoElem.addEventListener("pause", pauseHandle);
+	socket.on("pause", onPaues);
+}
+
+
+
+if(false) {
+	videoElem.addEventListener("seeked", app.handle.seekedHandle);
+	
+	if(false) {
+		socket.on("seeked", time => {
+			const videoElem = app.get.videoElem();
+			videoElem.pause();
+			setTimeout(() => {
+				videoElem.currentTime = time;
+			}, 200)
+			// new Promise((res, rej) => {
+			// 	res();
+			// }).then(() => {
+			// });
+			// videoElem.paused
+		});
+	}
+	
+	if(false) {
+		socket.on("seeked", time => {
+			const videoElem = app.get.videoElem();
+			videoElem.pause();
+			if(time === undefined) {
+				videoElem.addEventListener("seeked", app.handle.seekedHandle);
+			}
+			else {
+				videoElem.removeEventListener("seeked", app.handle.seekedHandle);
+				videoElem.currentTime = time;
+			}
+		});
+	}
+}
+
+
+if(false) {
+	videoElem.addEventListener("seeked", e => {
+		socket.emit("seeked", {
+			id: socket.id,
+			time: e.target.currentTime
+		});
+	});
+	socket.on("seeked", info => {
+		if(info.isUserControl) {
+			app.get.videoElem().currentTime = info.time;
+		}
+	});
+}
+
+if(false) {
+	if(false) {
+		function seekedHandle(e) {
+			socket.emit("seeked", {
+				count: 0, 
+				time: e.target.currentTime
+			});
+		}
+		videoElem.addEventListener("seeked", seekedHandle);
+		socket.on("seeked", info => {
+			info.count++;
+			console.log(info.count);
+			if(info.count <= 6) {
+				switch(info.count) {
+					case 2:
+						videoElem.pause();
+						videoElem.removeEventListener("seeked", seekedHandle);
+						videoElem.currentTime = info.time;
+					break;
+					// case 4:
+					case 6:
+						videoElem.addEventListener("seeked", seekedHandle);
+					break;
+				}
+				socket.emit("seeked", info);
+			}
+		});
+	}
+	
+	function seekedHandle(e) {
+		socket.emit("seeked", e.target.currentTime);
+	}
+	videoElem.addEventListener("seeked", seekedHandle);
+	async function onSeeked(time) {
+		await videoElem.pause();
+		// videoElem.removeEventListener("seeked", seekedHandle);
+		videoElem.currentTime = time;
+		// videoElem.addEventListener("seeked", seekedHandle);
+	}
+	socket.on("seeked", onSeeked);
+}
+
--- src/server.js
+++ src/server.js
@@ -16,6 +16,9 @@ const httpServer = http.createServer(app),
 wsServer = SocketIO(httpServer),
 roomName = "room";
 
+let prevSocketId = "";
+
+
 wsServer.on("connection", socket => {
 	socket.on("join", () => {
 		socket.join(roomName);
@@ -23,6 +26,98 @@ wsServer.on("connection", socket => {
 	socket.on("click", info => {
 		socket.to(roomName).emit("boxClick", info);
 	});
+	if(true) {
+	
+		socket.on("play", () => {
+			console.log(1);
+			socket.broadcast.emit("play");
+		});
+		socket.on("pause", () => {
+			console.log(2);
+			socket.broadcast.emit("pause");
+		});
+	}
+	if(false) {
+		let limit = 0;
+		function onPlay() {
+			console.log("play");
+			if(limit <= 3) {
+				socket.to(roomName).emit("play");
+			}
+			else {
+				socket.to(roomName).emit("play");
+				socket.off("play", onPlay);
+				socket.on("play", onPlay);
+				limit = 0;
+			}
+			console.log(limit);
+			limit++;
+		}
+		function onPause() {
+			console.log("pause");
+			if(limit <= 3) {
+				socket.to(roomName).emit("pause");
+			}
+			else {
+				socket.to(roomName).emit("pause");
+				socket.off("pause", onPause);
+				socket.on("pause", onPause);
+				limit = 0;
+			}
+		}
+		
+		socket.on("play", onPlay);
+		socket.on("pause", onPause);
+	}
+
+
+	if(false) {
+		socket.on("seeked", time => {
+			socket.to(roomName).emit("seeked", time);
+		});
+		socket.on("seeked", () => {
+			socket.to(roomName).emit("seeked");
+		});
+	}
+
+	if(false) {
+		socket.on("seeked", info => {
+			socket.to(roomName).emit("seeked", {
+				time: info.time,
+				isUserControl: info.id === prevSocketId
+			});
+			prevSocketId = info.id;
+		});
+		// socket.on("seeked", info => {
+		// 	socket.to(roomName).emit("seeked", {
+		// 		time: info.time,
+		// 		isUserControl: false
+		// 	});
+		// });
+	}
+	if(false) {
+		socket.on("seeked", info => {
+			info.count++;
+			if(info.count <= 6) {
+				console.log(info.count);
+				socket.to(roomName).emit("seeked", info);
+			}
+		});
+	}
+	if(false) {
+		let count = 0;
+		socket.on("seeked", time => {
+			console.log(count, time);
+			if(count < 2) {
+				socket.to(roomName).emit("seeked", time);
+			}
+			else {
+				count = 0;
+			}
+			count++;
+		}); 
+	}
+
 });
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
